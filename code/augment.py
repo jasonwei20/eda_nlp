2fe06d7e4dfd8ebe00a0ce35dc33c12e70bff5f8
Bruno Guimarães <brunobastosg@gmail.com>
2020-09-10 14:05:20 -0300

    Fixing argument parser and adding validation

--- code/augment.py
+++ code/augment.py
@@ -30,23 +30,26 @@ if args.num_aug:
 
 #how much to replace each word by synonyms
 alpha_sr = 0.1#default
-if args.alpha_sr:
+if args.alpha_sr is not None:
     alpha_sr = args.alpha_sr
 
 #how much to insert new words that are synonyms
 alpha_ri = 0.1#default
-if args.alpha_ri:
+if args.alpha_ri is not None:
     alpha_ri = args.alpha_ri
 
 #how much to swap words
 alpha_rs = 0.1#default
-if args.alpha_rs:
+if args.alpha_rs is not None:
     alpha_rs = args.alpha_rs
 
 #how much to delete words
 alpha_rd = 0.1#default
-if args.alpha_rd:
+if args.alpha_rd is not None:
     alpha_rd = args.alpha_rd
+
+if alpha_sr == alpha_ri == alpha_rs == alpha_rd == 0:
+     ap.error('At least one alpha should be greater than zero')
 
 #generate more data with standard augmentation
 def gen_eda(train_orig, output_file, alpha_sr, alpha_ri, alpha_rs, alpha_rd, num_aug=9):
